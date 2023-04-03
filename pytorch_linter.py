c1dda5b21c94983607885d3901cc7a8ffdcb1500
ashishdatta <ashishbdatta@gmail.com>
2023-04-02 22:02:04 -0700

    C4403 check if torch.distributed is check for

--- pytorch_linter.py
+++ pytorch_linter.py
@@ -6,6 +6,7 @@ from pylint.interfaces import IAstroidChecker
 
 class DDPImportChecker(BaseChecker):
     __implements__ = IAstroidChecker
+    __torch_distributed_used__ = False
 
     name = "ddp-import"
     priority = -1
@@ -20,11 +21,34 @@ class DDPImportChecker(BaseChecker):
                 "data-parallel-import",
                 "It is recommended to use DataDistributedParallel rather than DataParallel",
                 ),
+            "C4403": (
+                "torch.distributed is import and used but not checked if available",
+                "torch-distributed-is-available",
+                "It is recommended to use check if distributed is available",
+                ),
             }
     options = {}
-
-    def visit_import(self, node: astroid.Import):
+    def visit_import(self, module: astroid.Import):
         pass
+
+    def visit_module(self, module: astroid.Module):
+        _torch_distributed_used = False
+        check_if_torch_dist_is_available = False
+        for node in module.body:
+            if isinstance(node, astroid.nodes.Import):
+                for name, _ in node.names:
+                    if name == "torch.distributed":
+                        _torch_distributed_used = True
+            if _torch_distributed_used == True:
+                if isinstance(node, astroid.nodes.FunctionDef):
+                    for n in node.body:
+                        if isinstance(n, astroid.nodes.Expr):
+                            if n.value.func.attrname != "is_available":
+                                check_if_torch_dist_is_available = False
+                            elif n.value.func.attrname == "is_available":
+                                check_if_torch_dist_is_available = True
+        if check_if_torch_dist_is_available == False:
+            self.add_message("torch-distributed-is-available", node=node)
 
 
     def visit_importfrom(self, node:astroid.ImportFrom):
